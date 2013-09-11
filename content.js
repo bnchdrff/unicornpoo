d7eae0cb0ac8619518feb7873a0011e72d250d63
Benjamin Chodoroff <ben@theworkdept.com>
2013-09-11 11:52:13 -0400

    refs #3271: sample links to store items

--- content.js
+++ content.js
@@ -40,7 +40,7 @@ var PooContent = {
         'name': 'keys'
       },
       'footerboxes': [
-        'This work is licensed under a <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.</a>'
+        '<ul><li>Buy things at the AMP Store:</li><li><a href="https://store.alliedmedia.org/transformative-arts-practice-space-prints-moon-in-femme"><img src="https://store.alliedmedia.org/media/catalog/product/cache/1/thumbnail/60x60/9df78eab33525d08d6e5fb8d27136e95/m/o/moon_in_femme2.jpg"></a><a href="https://store.alliedmedia.org/fierce-fashion-future-totes"><img src="https://store.alliedmedia.org/media/catalog/product/cache/1/thumbnail/60x60/9df78eab33525d08d6e5fb8d27136e95/f/f/fff_tote_bag_pic.jpg"></a></li><li><a href="https://store.alliedmedia.org/amc2012kitofpartst"><img src="https://store.alliedmedia.org/media/catalog/product/cache/1/thumbnail/60x60/9df78eab33525d08d6e5fb8d27136e95/a/m/amctshirt-07.jpg"></a></li><li>This work is licensed under a <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.</a></li></ul>'
       ]
     },
     'amphome': {
